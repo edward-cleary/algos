d6af670a5fcdde91bd7a87e43bad29b9114c7435
Edward Cleary <eddiejcleary@gmail.com>
2022-05-15 14:12:29 -0600

    first commit

---
+++ src/main/java/sorting/InsertionSort.java
@@ -0,0 +1,38 @@
+package sorting;
+
+public class InsertionSort {
+/*
+    - In-place algorithm
+    - O(n2) time complexity - quadratic
+    - It will take 100 steps to sort 10 items, 10,000 steps to sort 100 items, 1,000,000 steps to sort 1000 items
+    - Stable algorithm
+*/
+    public static void main(String[] args) {
+        int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
+            int newElement = intArray[firstUnsortedIndex];
+
+            int i;
+
+            for (i = firstUnsortedIndex; i > 0 && intArray[i - 1]  > newElement; i--) {
+                intArray[i] = intArray[i - 1];
+            }
+
+            intArray[i] = newElement;
+        }
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        if (i == j) {
+            return;
+        }
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
