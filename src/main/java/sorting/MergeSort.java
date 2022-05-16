78920f401437f06b1f19024b5277578b3252c090
Edward Cleary <eddiejcleary@gmail.com>
2022-05-15 20:37:27 -0600

    mergesort and notes

--- src/main/java/sorting/MergeSort.java
+++ src/main/java/sorting/MergeSort.java
@@ -28,6 +28,8 @@ class MergeSort {
     public static void main(String[] args) {
         int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
 
+        mergeSort(intArray, 0, intArray.length);
+
         print(intArray);
     }
 
@@ -38,7 +40,28 @@ class MergeSort {
 
         int mid = (start + end) / 2;
 
+        mergeSort(input, start, mid);
+        mergeSort(input, mid, end);
+        merge(input, start, mid, end);
+    }
 
+    public static void merge(int[] input, int start, int mid, int end) {
+        if (input[mid - 1] <= input[mid]){
+            return;
+        }
+
+        int i = start;
+        int j = mid;
+        int tempIndex = 0;
+
+        int[] temp = new int[end - start];
+
+        while (i < mid && j < end) {
+            temp[tempIndex++] = input[i] <= input[j] ? input[i++] : input[j++];
+        }
+
+        System.arraycopy(input, i, input, start + tempIndex, mid - i);
+        System.arraycopy(temp, 0, input, start, tempIndex);
     }
 
     public static void print(int[] intArray) {
